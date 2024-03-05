d55f4a23134168dd831943247ad1c062560e5ec5
yuliiakanifolska <juliyakanifolskaya722@gmail.com>
2024-03-05 22:51:00 +0200

    Laba1.3

---
+++ task 1.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть числа:");
+        string input = Console.ReadLine();
+
+        
+        string[] numString = input.Split(' ');
+
+        
+        int[] num = numString.Select(int.Parse).ToArray();
+
+
+        int firstDodat = num.FirstOrDefault(c => c > 0);
+        int lastVid = num.LastOrDefault(c => c < 0);
+
+        Console.WriteLine("Перший додатній елемент: " + firstDodat);
+        Console.WriteLine("Останній від'ємний елемент: " + lastVid);
+        Console.ReadKey();
+    }
+}
